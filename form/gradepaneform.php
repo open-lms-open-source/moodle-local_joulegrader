ffd314456c1fa6ad5f865f4eb3a9fa2bcba7d3b5
Sam Chaffee <sam@moodlerooms.com>
2012-12-03 12:03:30 -0500

    INT-4105, INT-4212: Fixed override checkbox and message missing from simple grading grade pane in Joule Grader

--- form/gradepaneform.php
+++ form/gradepaneform.php
@@ -62,6 +62,12 @@ class local_joulegrader_form_gradepaneform extends moodleform {
             $mform->setDefault('grade', $grade);
         }
 
+        //check for override
+        if ($this->_customdata->has_override()) {
+            //if overridden in gradebook, add a checkbox
+            $mform->addElement('checkbox', 'override', null, get_string('overridetext', 'local_joulegrader'));
+        }
+
         $buttonarray = array();
         $buttonarray[] = &$mform->createElement('submit', 'submit', get_string('save', 'local_joulegrader'));
         if (!empty($nextuserid)) {
