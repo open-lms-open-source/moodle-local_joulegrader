03e3f9ba01d3ec3bfeeee60a84ad2fed3e7b4568
Mark Nielsen <mark@moodlerooms.com>
2015-06-05 10:17:49 -0700

    INT-7734: Prepare Joule Grader for OS

--- db/access.php
+++ db/access.php
@@ -1,12 +1,29 @@
 <?php
-defined('MOODLE_INTERNAL') or die();
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * joule Grader capability definitions
  *
- * @author Sam Chaffee
- * @package local/joulegrader
+ * @author    Sam Chaffee
+ * @package   local_joulegrader
+ * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') or die();
 
 $capabilities = array(
     'local/joulegrader:grade' => array(
--- version.php
+++ version.php
@@ -1,9 +1,36 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * joule Grader Version file
  *
- * @author Sam Chaffee
- * @package local/joulegrader
+ * @author    Sam Chaffee
+ * @package   local_joulegrader
+ * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013053000;
+/** @var object $plugin */
+$plugin->version      = 2013053000;
+$plugin->requires     = 2014111000;
+$plugin->component    = 'local_joulegrader';
+$plugin->release      = '2.8.0 (Build: 20150605)';
+$plugin->maturity     = MATURITY_STABLE;
+$plugin->dependencies = array(
+    'mod_hsuforum' => ANY_VERSION,
+    'mod_assign'   => ANY_VERSION,
+    'local_mr'     => ANY_VERSION,
+);
