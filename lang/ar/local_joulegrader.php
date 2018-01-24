7966138162575a161de713c8b0d644f1f30c2aac
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/local_joulegrader.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'مصنف التقدير لـ Joule';
+$string['joulegrader:grade'] = 'يعمل التقدير من خلال مصنف التقدير لـ Joule';
+$string['joulegrader:view'] = 'عرض العمل المُقدر من خلال مصنف التقدير لـ Joule';
+$string['gradebook'] = 'دفتر التقديرات';
+$string['nothingtodisplay'] = 'لا يوجد شيء ليتم عرضه';
+$string['needsgrading'] = 'إظهار الأنشطة التي تتطلب التقدير';
+$string['allactivities'] = 'إظهار جميع الأنشطة';
+$string['mobilenotsupported'] = 'لا يدعم "مصنف التقدير لـ Joule" مستعرضات الأجهزة المحمولة حاليًا';
+$string['exitfullscreen'] = 'إنهاء وضع ملء الشاشة';
+$string['fullscreen'] = 'وضع ملء الشاشة';
+$string['returncourse'] = 'الرجوع إلى المقرر الدراسي';
+$string['grading'] = 'التقدير';
+$string['nogradeableareas'] = 'لا توجد أنشطة قابلة للتقدير';
+$string['nogradeableusers'] = 'لا يوجد مستخدمين قابلين للتقدير';
+$string['showonlyuserposts'] = 'إظهار مشاركات المستخدم فقط';
+$string['groupbydiscussion'] = 'التجميع حسب المناقشة';
+$string['activity'] = 'النشاط القابل للتقدير';
+$string['activitynav'] = 'الأنشطة القابلة للتقدير';
+$string['activitynav_help'] = 'استخدم عنصر واجهة المستخدم هذا لتحديد النشاط القابل للتقدير المراد تقديره.';
+$string['group'] = 'المجموعة';
+$string['groupnav'] = 'المجموعات';
+$string['groupnav_help'] = 'استخدم عنصر واجهة المستخدم هذا لتحديد مجموعة.';
+$string['user'] = 'المستخدم';
+$string['usernav'] = 'المستخدمون';
+$string['usernav_help'] = 'استخدم عنصر واجهة المستخدم هذا لتحديد المستخدم المراد تقديره.';
+$string['navviewlabel'] = 'عرض {$a}';
+$string['commentdeleted'] = 'تم حذف المشاركة من قبل المستخدم {$a->deletedby} في {$a->deletedon}';
+$string['deletecomment'] = 'حذف تعليق تم وضعه في {$a}';
+$string['previous'] = 'السابق {$a}';
+$string['next'] = 'التالي {$a}';
+$string['assignmentavailable'] = 'متاح';
+$string['on'] = 'في {$a}';
+$string['until'] = 'حتى {$a}';
+$string['lastedited'] = 'آخر تحرير في {$a}';
+$string['assign23-latesubmission'] = 'تأخر {$a} عن هذا التقديم.';
+$string['assign23-userextensiondate'] = 'تم التمديد حتى: {$a}';
+$string['downloadall'] = 'تنزيل جميع الملفات';
+$string['download'] = 'تنزيل';
+$string['viewinline'] = 'عرض مضمن';
+$string['activitycomments'] = 'تعليقات النشاط';
+$string['overallfeedback'] = 'الملاحظات الكلية';
+$string['filefeedback'] = 'ملاحظات الملف';
+$string['attemptnumber'] = 'المحاولة {$a->attemptnumber}: {$a->attempttime}';
+$string['viewingattempt'] = 'عرض المحاولة';
+$string['attemptstatus'] = 'قام الطالب بإجراء {$a->number} من {$a->outof} محاولة.';
+$string['assignmentstatus'] = 'حالة الواجب';
+$string['unlimited'] = 'غير محدود';
+$string['gradebookgrade'] = 'التقدير الحالي في دفتر التقديرات';
+$string['attemptgrade'] = 'تقدير المحاولة';
+$string['gradeoutof'] = 'التقدير (خارج {$a})';
+$string['gradeoutofrange'] = 'التقدير خارج النطاق';
+$string['overridetext'] = 'قام مدرس في وقت سابق بإنشاء تقدير لهذا النشاط مباشرة في دفتر التقديرات.  حدد هذا المربع إذا كنت ترغب في استبدال هذا التقدير أيضًا.';
+$string['save'] = 'حفظ التقدير';
+$string['saveandnext'] = 'حفظ التقدير والتالي';
+$string['gradingdisabled'] = 'تم قفل تقدير هذا النشاط. لتمكين التقدير يرجى فك قفل التقدير بواسطة دفتر التقديرات.';
+$string['applytoall'] = 'تطبيق التقديرات والملاحظات على المجموعة بالكامل';
+$string['applytoall_help'] = 'إذا تم تحديد "نعم" سيتلقى جميع أعضاء المجموعة التقدير والملاحظات بغض النظر عن أي تقدير أو ملاحظات موجودة في دفتر التقديرات.';
+$string['criteria'] = 'المعايير';
+$string['checklist'] = 'قائمة الاختيارات';
+$string['gradesaved'] = 'تم تحديث التقدير بنجاح';
+$string['gradesavedx'] = '{$a} تم تحديث التقديرات بنجاح';
+$string['couldnotsave'] = 'تعذر تحديث التقدير';
+$string['couldnotsavex'] = 'تعذر تحديث تقدير {$a}';
+$string['notgraded'] = 'لم يتم تقدير الواجب';
+$string['viewchecklistteacher'] = 'التقدير باستخدام قائمة الاختيارات';
+$string['viewrubricteacher'] = 'التقدير باستخدام القاعدة';
+$string['viewcheckliststudent'] = 'عرض قائمة اختيار التقديرات';
+$string['viewrubricstudent'] = 'عرض قاعدة التقدير';
+$string['viewguidestudent'] = 'عرض دليل إعطاء علامة التقدير';
+$string['viewguideteacher'] = 'التقدير باستخدام دليل إعطاء العلامة';
+$string['guide'] = 'دليل إعطاء علامة';
+$string['rubric'] = 'سلم التقدير الوصفي';
+$string['rubricerror'] = 'يرجى تحديد مستوى واحد لكل معيار';
+$string['guideerror'] = 'يرجى توفير تقدير صالح لكل معيار';
+$string['score'] = 'درجة';
+$string['gradeoverriddenstudent'] = '(تجاوز في دفتر التقديرات: {$a})';
+$string['close'] = 'إغلاق';
+$string['allfiles'] = 'جميع الملفات';
+$string['add'] = 'حفظ التعليق';
+$string['attachments'] = 'المرفقات';
+$string['commentrequired'] = 'مطلوب تعليق';
+$string['commentloop'] = 'حلقة التعليقات';
+$string['notreleased'] = 'لم يتم إصدار تقدير الواجب بعد';
+$string['eventgraderviewed'] = 'تم عرض مصنف التقدير لـ Joule';
+$string['eventactivitygraded'] = 'تم تقدير النشاط في مصنف التقدير لـ Joule';
+$string['eventcommentdeleted'] = 'تم حذف تعليق في مصنف التقدير لـ Joule';
+$string['eventcommentadded'] = 'تم إضافة تعليق في مصنف التقدير لـ Joule';
