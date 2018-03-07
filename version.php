dbcbd3a6732edd0a5ff9179a5a59c822e18aede0
Mark Nielsen <mark@moodlerooms.com>
2018-03-07 12:12:13 -0800

    INT-12974: bump plugin release info

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 /** @var object $plugin */
 $plugin->version      = 2017120800;
-$plugin->requires     = 2017051500;
+$plugin->requires     = 2017111300;
 $plugin->component    = 'local_joulegrader';
-$plugin->release      = '3.3.3';
+$plugin->release      = '3.4.1';
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_hsuforum' => ANY_VERSION,
