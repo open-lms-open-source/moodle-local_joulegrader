4e8824781e5538895b414b104704d84ea6e8a369
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:11 +0000

    INT-13192: Importing de lang

---
+++ lang/de/local_joulegrader.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moodlerooms-Bewertungsfunktion';
+$string['joulegrader:grade'] = 'Arbeit mit der Moodlerooms-Bewertungsfunktion bewerten';
+$string['joulegrader:view'] = 'Bewertete Arbeit mit der Moodlerooms-Bewertungsfunktion anzeigen';
+$string['gradebook'] = 'Bewertungsbericht';
+$string['nothingtodisplay'] = 'Nichts anzuzeigen';
+$string['needsgrading'] = 'Aktivitäten anzeigen, die bewertet werden müssen';
+$string['allactivities'] = 'Alle Aktivitäten anzeigen';
+$string['mobilenotsupported'] = 'Die Moodlerooms-Bewertungsfunktion unterstützt derzeit keine mobilen Browser';
+$string['exitfullscreen'] = 'Vollbildmodus verlassen';
+$string['fullscreen'] = 'Vollbildmodus';
+$string['returncourse'] = 'Zurück zum Kurs';
+$string['grading'] = 'Wird bewertet';
+$string['nogradeableareas'] = 'Keine bewertbaren Aktivitäten';
+$string['nogradeableusers'] = 'Keine bewertbaren Nutzer';
+$string['showonlyuserposts'] = 'Nur Einträge des Nutzers/der Nutzerin anzeigen';
+$string['groupbydiscussion'] = 'Nach Diskussion gruppieren';
+$string['activity'] = 'Bewertbare Aktivität';
+$string['activitynav'] = 'Bewertbare Aktivitäten';
+$string['activitynav_help'] = 'Verwenden Sie dieses Widget, um auszuwählen, welche bewertbare Aktivität bewertet werden soll.';
+$string['group'] = 'Gruppe';
+$string['groupnav'] = 'Gruppen';
+$string['groupnav_help'] = 'Verwenden Sie dieses Widget, um eine Gruppe auszuwählen.';
+$string['user'] = 'Nutzer/in';
+$string['usernav'] = 'Nutzer/innen';
+$string['usernav_help'] = 'Verwenden Sie dieses Widget, um auszuwählen, welcher Nutzer bewertet werden soll.';
+$string['navviewlabel'] = '{$a} anzeigen';
+$string['commentdeleted'] = 'Der Nutzer {$a->deletedby} hat den Eintrag am {$a->deletedon} gelöscht.';
+$string['deletecomment'] = 'Kommentar vom {$a} löschen.';
+$string['previous'] = 'Vorige {$a}';
+$string['next'] = 'Nächste {$a}';
+$string['assignmentavailable'] = 'Verfügbar';
+$string['on'] = 'am {$a}';
+$string['until'] = 'bis {$a}';
+$string['lastedited'] = 'Zuletzt bearbeitet am {$a}';
+$string['assign23-latesubmission'] = 'Die abgegebene Aufgabe war um {$a} verspätet.';
+$string['assign23-userextensiondate'] = 'Verlängerung möglich bis: {$a}';
+$string['downloadall'] = 'Alle Dateien herunterladen';
+$string['download'] = 'herunterladen';
+$string['viewinline'] = 'Direkt anzeigen';
+$string['activitycomments'] = 'Kommentare zur Aktivität';
+$string['overallfeedback'] = 'Gesamtfeedback';
+$string['filefeedback'] = 'Feedback als Datei';
+$string['attemptnumber'] = 'Versuch {$a->attemptnumber}: {$a->attempttime}';
+$string['viewingattempt'] = 'Versuch wird angezeigt';
+$string['attemptstatus'] = 'Teilnehmer/in hat {$a->number} von {$a->outof} Versuchen unternommen.';
+$string['assignmentstatus'] = 'Aufgabenstatus';
+$string['unlimited'] = 'Unbegrenzt';
+$string['gradebookgrade'] = 'Aktuelle Bewertung im Bewertungsbericht';
+$string['attemptgrade'] = 'Note für Testversuch';
+$string['gradeoutof'] = 'Bewertung (von {$a})';
+$string['gradeoutofrange'] = 'Bewertung liegt außerhalb des Bereichs.';
+$string['overridetext'] = 'Eine Lehrkraft hat bereits eine Bewertung für diese Aktivität direkt in den Bewertungsbericht eingetragen.  Aktivieren Sie dieses Kontrollkästchen, wenn Sie diese Bewertung auch ersetzen möchten.';
+$string['save'] = 'Bewertung speichern';
+$string['saveandnext'] = 'Bewertung speichern und weiter';
+$string['gradingdisabled'] = 'Das Bewerten dieser Aktivität ist gesperrt. Um das Bewerten zu aktivieren, geben Sie bitte die Bewertung über den Bewertungsbericht frei.';
+$string['applytoall'] = 'Bewertungen und Feedback der gesamten Gruppe zuweisen.';
+$string['applytoall_help'] = 'Wenn "Ja" ausgewählt ist, erhalten alle Gruppenmitglieder die Bewertung und das Feedback, unabhängig davon, ob in dem Bewertungsbericht bereits eine Bewertung oder ein Feedback vorhanden ist.';
+$string['criteria'] = 'Kriterien';
+$string['checklist'] = 'Checkliste';
+$string['gradesaved'] = 'Bewertung erfolgreich aktualisiert.';
+$string['gradesavedx'] = '{$a} Bewertungen erfolgreich aktualisiert.';
+$string['couldnotsave'] = 'Die Bewertung konnte nicht aktualisiert werden.';
+$string['couldnotsavex'] = 'Die Bewertung für {$a} konnte nicht aktualisiert werden.';
+$string['notgraded'] = 'Aufgabe nicht bewertet';
+$string['viewchecklistteacher'] = 'Mit Checkliste bewerten';
+$string['viewrubricteacher'] = 'Mit Rubrik bewerten';
+$string['viewcheckliststudent'] = 'Bewertungs-Checkliste anzeigen';
+$string['viewrubricstudent'] = 'Bewertungsrubrik anzeigen';
+$string['viewguidestudent'] = 'Bewertungsanleitung anzeigen';
+$string['viewguideteacher'] = 'Mit Bewertungsanleitung bewerten';
+$string['guide'] = 'Bewertungsrichtlinie';
+$string['rubric'] = 'Rubrik';
+$string['rubricerror'] = 'Wählen Sie bitte für jedes Kriterium eine Ebene aus.';
+$string['guideerror'] = 'Geben Sie bitte eine gültige Bewertung für jedes Kriterium ein.';
+$string['score'] = 'Bewertung';
+$string['gradeoverriddenstudent'] = '(Überschreiben in Bewertungsbericht: {$a})';
+$string['close'] = 'Schließen';
+$string['allfiles'] = 'Alle Dateien';
+$string['add'] = 'Kommentar speichern';
+$string['attachments'] = 'Anhänge';
+$string['commentrequired'] = 'Kommentar erforderlich';
+$string['commentloop'] = 'Kommentarschleife';
+$string['notreleased'] = 'Aufgabenbewertung noch nicht freigegeben';
+$string['eventgraderviewed'] = 'Moodlerooms-Bewertungsfunktion angezeigt';
+$string['eventactivitygraded'] = 'Aktivität in Moodlerooms-Bewertungsfunktion bewertet';
+$string['eventcommentdeleted'] = 'Kommentar in Moodlerooms-Bewertungsfunktion gelöscht';
+$string['eventcommentadded'] = 'Kommentar in Moodlerooms-Bewertungsfunktion hinzugefügt';
