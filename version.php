2848e639835867265a7a27977d93fb20d17d6253
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 /** @var object $plugin */
 $plugin->version      = 2018080800;
-$plugin->requires     = 2018120300;
+$plugin->requires     = 2019052000;
 $plugin->component    = 'local_joulegrader';
-$plugin->release      = '3.6.3';
+$plugin->release      = '3.7.3';
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_hsuforum' => ANY_VERSION,
