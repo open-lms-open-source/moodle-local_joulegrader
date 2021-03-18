4343911ff8f0f344a3a8a76f1ad437e8809fa6c5
Fabian Batioja <fabian.batioja@openlms.net>
2021-03-18 22:56:29 +0000

    INT-16713: Importing tr lang

--- lang/tr/local_joulegrader.php
+++ lang/tr/local_joulegrader.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2020 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2021 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,7 +24,7 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Open Grader';
 $string['joulegrader:grade'] = 'Open Grader aracılığıyla çalışmaya not ver';
 $string['joulegrader:view'] = 'Not verilen çalışmaya Open Grader aracılığıyla bak';
-$string['gradebook'] = 'Not Defteri';
+$string['gradebook'] = 'Not defteri';
 $string['nothingtodisplay'] = 'Görüntülenecek Bir Şey Yok';
 $string['needsgrading'] = 'Not Verilmesi Gereken Etkinlikleri Göster';
 $string['allactivities'] = 'Tüm Etkinlikleri Göster';
