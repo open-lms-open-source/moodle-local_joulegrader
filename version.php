333a8c4cc9f243d9d9cc3ab61399e7b4e3ddcf8c
Rafael Monterroza <rafael.monterroza@openlms.net>
2022-07-29 18:07:24 +0000

    INT-18181: bump versions for 3.11 mp4

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
  */
 
 /** @var object $plugin */
-$plugin->version      = 2022060600;
+$plugin->version      = 2022072900;
 $plugin->requires     = 2021051700;
 $plugin->component    = 'local_joulegrader';
-$plugin->release      = '3.11.3';
+$plugin->release      = '3.11.6';
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_hsuforum' => ANY_VERSION,
