702e4204fe51e4c65141adfba00779ba668f4fb2
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-06-17 16:37:58 -0500

    INT-19886: 4.3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
  */
 
 /** @var object $plugin */
-$plugin->version      = 2024040200;
-$plugin->requires     = 2022112800;
+$plugin->version      = 2024061700;
+$plugin->requires     = 2023100900;
 $plugin->component    = 'local_joulegrader';
-$plugin->release      = '4.1.3';
+$plugin->release      = '4.3.4';
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_hsuforum' => ANY_VERSION,
