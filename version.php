fed6487494739129575d792db79548ebd2ff3660
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-07-27 10:04:58 -0500

    INT-20243: 4.3 MP1 Production Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 /** @var object $plugin */
-$plugin->version      = 2024071800;
+$plugin->version      = 2024072700;
 $plugin->requires     = 2023100900;
 $plugin->component    = 'local_joulegrader';
 $plugin->release      = '4.3.4';
